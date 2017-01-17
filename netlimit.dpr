19d14c003f709eb5af368cc149b2ea365a3de6c1
Stefan Ascher <stievie2006@gmail.com>
2017-01-17 20:38:11 +0100

    Minor tweaks

--- netlimit.dpr
+++ netlimit.dpr
@@ -37,6 +37,7 @@ var
   drops: integer;
   console: THandle;
   handle: THandle;
+  drop_all: boolean = false;
 
 function passthr(arg: Pointer): DWORD;
 var
@@ -59,10 +60,13 @@ begin
     if (sleep_ms > 0) then
       Sleep(sleep_ms);
     per := Random(100);
-    if (per <= drops) then begin
+
+    if ((drops > 0) and (per <= drops)) or drop_all then begin
       // Drop it
-      SetConsoleTextAttribute(console, FOREGROUND_GREEN or FOREGROUND_RED);
-      WriteLn(Format('Info: dropped packet (%d %%)', [per]));
+      if not drop_all then begin
+        SetConsoleTextAttribute(console, FOREGROUND_GREEN or FOREGROUND_RED);
+        WriteLn(Format('Info: dropped packet (%d%%)', [per]));
+      end;
       Continue;
     end;
 
@@ -142,6 +146,7 @@ var
   i: integer;
   thread_id: DWORD;
   key: Word;
+  cs: TRTLCriticalSection;
 begin
   WriteLn('netlimit, (c) 2017 sa');
   WriteLn('Simulate bad network');
@@ -169,27 +174,53 @@ begin
       Halt(1);
     end;
 
+    InitializeCriticalSection(cs);
     for i := 1 to num_threads do begin
       hThreads[i-1] := CreateThread(nil, 1, @passthr, nil, 0, thread_id);
       if (hThreads[i-1] = 0) then begin
         SetConsoleTextAttribute(console, FOREGROUND_RED);
-        WriteLn(Format('Error: failed to start passthru thread (%u)', [GetLastError]));
+        WriteLn(Format('Error: failed to start thread (%u)', [GetLastError]));
         Halt(1);
       end;
     end;
     SetConsoleTextAttribute(console, FOREGROUND_GREEN);
-    WriteLn('Running. Press `q` to terminate.');
+    WriteLn('Running. Press `q` to terminate, `h` for help.');
 
     while (true) do begin
       key := KeyPressed;
       case key of
         Ord('Q'):
           begin
+            // Quit
             Write('Really? [y/n]: ');
             key := KeyPressed;
             WriteLn(Char(key));
             if (key = Ord('Y')) then
               Break;
+          end;
+        Ord('D'):
+          begin
+            // Disconnect -> Drop all
+            EnterCriticalsection(cs);
+            try
+              drop_all := not drop_all;
+              if drop_all then begin
+                SetConsoleTextAttribute(console, FOREGROUND_RED);
+                WriteLn('Disconnected, packet loss = 100%');
+              end else begin
+                SetConsoleTextAttribute(console, FOREGROUND_GREEN);
+                WriteLn(Format('Connected, packet loss = %d%%', [drops]));
+              end;
+            finally
+              LeaveCriticalsection(cs);
+            end;
+          end;
+        Ord('H'):
+          begin
+            SetConsoleTextAttribute(console, FOREGROUND_RED or FOREGROUND_GREEN or FOREGROUND_BLUE);
+            WriteLn('q: Quit');
+            WriteLn('d: Toggle drop all packets');
+            WriteLn('h: Show this help');
           end;
       end;
       Sleep(10);
@@ -200,6 +231,8 @@ begin
     for i := 1 to num_threads do begin
       TerminateThread(hThreads[i-1], 0);
     end;
+
+    DeleteCriticalSection(cs);
     WinDivertClose(handle);
     SetConsoleTextAttribute(console, FOREGROUND_RED);
     WriteLn('Hasta la vista, baby');
