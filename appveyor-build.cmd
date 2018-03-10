410ab14d98acf31b1eae2bc290ebbdfc919e0518
Solodyagin <solodyagin@gmail.com>
2018-03-10 11:20:12 +0300

    AppVeyor: install Lazarus v1.8.2

--- appveyor-build.cmd
+++ appveyor-build.cmd
@@ -1,5 +1,5 @@
 @echo off
-set lazbuild=c:\lazarus\lazbuild
+set lazbuild=c:\lazarus\lazbuild.exe
 for /F "tokens=*" %%i in ('dir /A:-D /B /S "*.lpi"') do (
 	"%lazbuild%" "%%i" --build-all --build-mode=Win32-Release
 	"%lazbuild%" "%%i" --build-all --build-mode=Win64-Release
